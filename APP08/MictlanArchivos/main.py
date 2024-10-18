a5962a02b223af4f496d68fb374c3590d0db637d
sofiagarcia27 <garciamorenosofiapaulina@cetis50cdmx.com>
2024-10-17 19:55:26 -0600

    app 08

---
+++ APP08/MictlanArchivos/main.py
@@ -0,0 +1,11 @@
+import flet as ft
+
+
+def main(page  ft Page):
+    page.window_width=800
+    page.window_height=800
+    
+    page.bgcolor="purple"
+    page.title="Mictlan"
+    page.fgcolor="grey"
+    
